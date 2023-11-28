1d0fdd08c936eb8fc9c5b867d9f3aeb8dc965769
Vikkans <Jakobsenviktor6@gmail.com>
2023-11-28 18:27:41 +0100

    Add project files.

---
+++ ConsoleApp50/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Consoleapp50
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tal = new int[3];
+            tal[0] = 24;
+            tal[1] = 23;
+
+            Console.WriteLine("Välj det tredje talet");
+            tal[2] = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(tal[0] + " " + tal[1] + " " + tal[2]);
+            
+        }
+    }
+}
